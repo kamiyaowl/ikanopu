6460ac40b82bfe287ebd8bad1ff6eb06611bacab
K <fieldprogrammable@gmail.com>
2019-01-17 00:50:22 +0900

    コマンドヘルプを出せるようになった

--- ikanopu/Module/PuService.cs
+++ ikanopu/Module/PuService.cs
@@ -14,18 +14,25 @@ using System.Threading.Tasks;
 namespace ikanopu.Module {
     public class PuService : ModuleBase {
         public DiscordSocketClient Discord { get; set; }
+        public CommandService CommandService { get; set; }
         public ImageProcessingService ImageProcessingService { get; set; }
 
-        [Command("pu")]
-        [Alias("ikanopu")]
+        [Command("pu"), Summary("コマンド一覧を表示します")]
+        [Alias("ikanopu", "pu help", "ikanopu help")]
         public async Task Help() {
             var sb = new StringBuilder();
-            sb.AppendLine("ikanopu(beta)");
+            sb.AppendLine("*ikanopu(beta)*");
             sb.AppendLine("プライベートマッチの音声チャンネル遷移を自動でやってくれるかも");
-            await ReplyAsync(sb.ToString());
+            sb.AppendLine();
+            sb.AppendLine("コマンドは先頭に`!`をつけた後に以下リストにあるものが使用できます");
+            var builder = new EmbedBuilder();
+            foreach (var c in CommandService.Commands) {
+                builder.AddField(c.Name, c.Summary ?? "no description");
+            }
+            await ReplyAsync(sb.ToString(), false, builder.Build());
         }
 
-        [Command("pu capture")]
+        [Command("pu capture"), Summary("現在のキャプチャデバイスの画像を取得します")]
         public async Task Capture() {
             Mat mat = null;
             lock (ImageProcessingService.CaptureRawMat) {
@@ -37,11 +44,11 @@ namespace ikanopu.Module {
 
             await Context.Channel.SendFileAsync("capture.jpg", "capture.jpg");
         }
-        [Command("pu show config")]
+        [Command("pu show config"), Summary("config.jsonの内容を表示します")]
         public async Task ShowConfig() {
             await ReplyAsync($"```\n{JsonConvert.SerializeObject(ImageProcessingService.Config, Formatting.None)}\n```");
         }
-        [Command("pu echo")]
+        [Command("pu echo"), Summary("俺がオウムだ")]
         public async Task Echo([Remainder] string text) {
             await ReplyAsync($"\u200B{text}");
         }
