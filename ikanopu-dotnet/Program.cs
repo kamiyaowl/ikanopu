16d46b16f21a78c41b58dae67988cdacf70285fd
K <fieldprogrammable@gmail.com>
2019-02-02 20:23:28 +0900

    OpenCVSharpうごくじゃん

--- ikanopu-dotnet/Program.cs
+++ ikanopu-dotnet/Program.cs
@@ -1,9 +1,20 @@
-﻿using System;
+﻿using OpenCvSharp;
+using System;
 
 namespace ikanopu_dotnet {
     class Program {
         static void Main(string[] args) {
-            Console.WriteLine("Hello World!");
+            using (var mat = new Mat(1080, 1920, MatType.CV_8UC3))
+            using (var captureRawWindow = new Window("Capture Raw", WindowMode.KeepRatio))
+            using (var capture = new VideoCapture(0) { }) {
+                capture.FrameWidth = 1920;
+                capture.FrameHeight = 1080;
+
+                while (Cv2.WaitKey(1) == -1) {
+                    capture.Read(mat);
+                    captureRawWindow.ShowImage(mat);
+                }
+            }
         }
     }
 }
